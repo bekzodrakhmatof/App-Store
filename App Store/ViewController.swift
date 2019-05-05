7e93670f9fde8e98d8da3ae78734b38a5c288dcc
Bekzod <rakhmatovbekzod1996@gmail.com>
2019-05-06 01:51:19 +0900

    Initial Commit

---
+++ App Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Store
+//
+//  Created by Bekzod Rakhmatov on 06/05/2019.
+//  Copyright © 2019 BekzodRakhmatov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
